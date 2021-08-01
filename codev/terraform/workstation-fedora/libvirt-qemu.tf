ab5d3fd25a58e8a448f41a79882ea1e952df7fc9
Creativ <vietbao.telescope@gmail.com>
2021-08-01 09:52:08 +0700

    terraform: update libvirt deployment

--- codev/terraform/workstation-fedora/libvirt-qemu.tf
+++ codev/terraform/workstation-fedora/libvirt-qemu.tf
@@ -1,10 +1,11 @@
 locals {
   DOMAIN_NAME                = "workstation-fedora"
   FEDORA_VERSION             = 32
-  ISO_DISK                   = pathexpand("~/Downloads/Fedora-Workstation-Live-x86_64-32-1.6.iso")
+  POOL_PATH                  = pathexpand("/datum/vdisk")
+  ISO_DISK                   = pathexpand("/datum/vdisk/Fedora-Workstation-Live-x86_64-32-1.6.iso")
   SYSTEM_DISK_SIZE_IN_BYTE   = 8000000
   SYSTEM_DISK_REFERENCE      = "false"
-  SYSTEM_DISK_REFERENCE_PATH = pathexpand("~/echo-home/codev/terraform/workstation-fedora/workstation-fedora.img")
+  SYSTEM_DISK_REFERENCE_PATH = pathexpand("/datum/vdisk/workstation-fedora-32.raw.img")
 }
 
 
@@ -20,18 +21,25 @@ resource "random_string" "suffix" {
   special = false
 }
 
+resource "libvirt_pool" "vdisk" {
+  name = "vdisk"
+  type = "dir"
+  path = local.POOL_PATH
+}
 
 resource "libvirt_volume" "system_volume" {
   count  = local.SYSTEM_DISK_REFERENCE ? 0 : 1
   name   = "${local.DOMAIN_NAME}-${local.FEDORA_VERSION}-${random_string.suffix.id}"
   size   = local.SYSTEM_DISK_SIZE_IN_BYTE
   format = "raw"
+  pool   = libvirt_pool.vdisk.name
 }
 
 resource "libvirt_volume" "system_volume_reference" {
   count  = local.SYSTEM_DISK_REFERENCE ? 1 : 0
   name   = "${local.DOMAIN_NAME}-${local.FEDORA_VERSION}-${random_string.suffix.id}"
   source = local.SYSTEM_DISK_REFERENCE_PATH
+  pool   = libvirt_pool.vdisk.name
 }
 
 
@@ -50,6 +58,11 @@ resource "libvirt_domain" "workstation_fedora" {
   disk {
     volume_id = local.SYSTEM_DISK_REFERENCE ? libvirt_volume.system_volume_reference[0].id : libvirt_volume.system_volume[0].id
     scsi      = true
+  }
+
+  network_interface {
+    network_name   = "default"
+    wait_for_lease = false
   }
 }
 
