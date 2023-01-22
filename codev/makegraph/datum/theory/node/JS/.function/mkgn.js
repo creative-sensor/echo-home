700ea3d8af9943cc18f80d82782e067475325b4d
Creativ <vietbao.telescope@gmail.com>
2023-01-22 10:53:02 +0700

    MKG: nodejs driver

---
+++ codev/makegraph/datum/theory/node/JS/.function/mkgn.js
@@ -0,0 +1,4 @@
+
+exports.epoch_time = function () {
+  return Math.floor(Date.now()/1000);
+};
---
+++ codev/makegraph/datum/theory/node/JS/.function/start.js
@@ -0,0 +1,3 @@
+var mkgn = require('./mkgn');
+
+console.log(mkgn.epoch_time() + ': { "mkgn":"----check-in----"}')
