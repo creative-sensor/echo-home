66571970dcaf1bfeca51d0814e5b48aecece6a77
Creativ <vietbao.telescope@gmail.com>
2023-01-31 11:01:45 +0700

    misc
    - gpull

--- codev/makegraph/datum/theory/node/P/.function/mkgn.py
+++ codev/makegraph/datum/theory/node/P/.function/mkgn.py
@@ -1,1 +1,27 @@
-/home/creativ/echo-home/codev/makegraph/.template/nodeX_python/.function/mkgn.py
+
+import sys
+import os
+sys.path.append('./modules/')
+
+import yaml
+import json
+import time
+# ----
+def yson(namespace,dict_block,file):
+    print(namespace + ": " + json.dumps(dict_block), file=file)
+
+def stdlog(event):
+    epoch_time = str(int(time.time()))
+    if not event is dict: event = '{"event":"' + str(event) + '"}'
+    print(epoch_time + ": " + str(event))
+# ----
+NAME = os.environ['MKGN_NAME']
+OUTPUT = open(os.environ['MKGN_OUTPUT'],'w')
+INPUT = {}
+list_subnode = os.environ['MKGN_INPUT'].split(':')[1].split()
+
+for node in list_subnode:
+    file = f'{os.environ["GRAPH_DIR"]}/{node}'
+    with open(file,'r') as f:
+        INPUT[node] = f.read()
+
