11b47f16fb5a21809ac168da780f90e499c133ab
Creativ <vietbao.telescope@gmail.com>
2024-02-10 15:55:31 +0700

    up

---
+++ windows/powershell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,7 @@
+function prompt{
+    Write-Host("$([char]0x264a)$([char]0x2648)" + $Env:UserName) -NoNewLine -ForegroundColor 2
+    Write-Host("@" + $Env:ComputerName +"] ") -NoNewLine -ForegroundColor 5
+    Write-Host([io.path]::GetFileNameWithoutExtension((pwd)) +">") -NoNewLine -ForegroundColor 6
+    return " "
+}
+
