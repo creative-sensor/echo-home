063594e2875d2d2120c3731bad6b3ed57c2f39a3
Creativ <vietbao.telescope@gmail.com>
2024-02-12 19:39:00 +0700

    MDOS

--- windows/powershell/.function/setup-initial.ps1
+++ windows/powershell/.function/setup-initial.ps1
@@ -1,1 +1,5 @@
-Set-ExecutionPolicy -Scope CurrentUser RemoteSigned
+#Set-ExecutionPolicy -Scope CurrentUser RemoteSigned
+
+$PROFILE_CUCH = "$HOME\Documents\WindowsPowerShell"
+if (-not (Test-Path -Path $PROFILE_CUCH)) { mkdir $PROFILE_CUCH }
+cp Microsoft.PowerShell_profile.ps1 $PROFILE_CUCH
