ec5e3b71862943cd7b6029f16d5899fbe008d75c
Creativ <vietbao.telescope@gmail.com>
2025-05-22 16:23:35 +0700

    DTUM-3

--- windows/agilite/distum/.function/part-guid.ps1
+++ windows/agilite/distum/.function/part-guid.ps1
@@ -3,4 +3,5 @@ $drive = $args[0]
 #$drive
 $drive_letter = @(echo $drive | grep -E -o '[a-zA-Z]+')
 #$drive_letter
-Get-Partition -DriveLetter $drive_letter | Select-Object Guid  |  ConvertTo-Json -Depth 1 | jq -r .Guid
+Get-Partition -DriveLetter $drive_letter | Select-Object -ExpandProperty Guid | awk -F'-' '{print $NF}' | grep  -E  -o '[a-f0-9]+'
+
