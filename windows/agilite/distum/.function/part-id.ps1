cb0edfa18ca032a41ca7ee1cf6f7f778f2ad1abd
Creativ <vietbao.telescope@gmail.com>
2025-05-24 11:56:40 +0700

    DTUM-3

--- windows/agilite/distum/.function/part-id.ps1
+++ windows/agilite/distum/.function/part-id.ps1
@@ -4,7 +4,7 @@ $drive = $args[0]
 $drive_letter = @(echo $drive | grep -E -o '[a-zA-Z]+')
 #$drive_letter
 
-$disk_id = @(Get-Partition -DriveLetter $drive_letter | Select-Object  -ExpandProperty DiskId | awk -F'-' '{print $NF}' | grep  -E  -o '[a-f0-9]+')
+$disk_id = @(Get-Partition -DriveLetter $drive_letter | Select-Object  -ExpandProperty AccessPaths | awk -F'-' '{print $NF}' | grep  -E  -o '[a-f0-9]+')
 $part_number = @(Get-Partition -DriveLetter D | Select-Object -ExpandProperty PartitionNumber)
 
 echo "$disk_id-$part_number"
