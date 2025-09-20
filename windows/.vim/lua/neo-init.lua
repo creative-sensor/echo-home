35ead9cc3df424a04e4bb4c0970b8345c42cc1a1
Creativ <vietbao.telescope@gmail.com>
2025-09-20 14:50:48 +0700

    up

--- windows/.vim/lua/neo-init.lua
+++ windows/.vim/lua/neo-init.lua
@@ -9,13 +9,15 @@ require('lualine').setup(
 local theme = {
   fill = 'TabLineFill',
   -- Also you can do this: fill = { fg='#f2e9de', bg='#907aa9', style='italic' }
-  head = 'TabLine',
-  current_tab = 'TabLineSel',
+  head = { fg='#ffffff', bg='#000000', style='italic' },
+  current_tab = { fg='#000000', bg='#f5006e', style='italic' },
   tab = 'TabLine',
   win = 'TabLine',
   tail = 'TabLine',
 }
 require('tabby').setup({
+
+
   line = function(line)
     return {
       {
@@ -26,30 +28,29 @@ require('tabby').setup({
         local hl = tab.is_current() and theme.current_tab or theme.tab
         return {
           line.sep('', hl, theme.fill),
-          tab.is_current() and '' or '󰆣',
-          tab.number(),
+          tab.is_current(),
           tab.name(),
-          tab.close_btn(''),
+--          tab.close_btn(''),
           line.sep('', hl, theme.fill),
           hl = hl,
-          margin = '',
-        }
-      end),
-      line.spacer(),
-      line.wins_in_tab(line.api.get_current_tab()).foreach(function(win)
-        return {
-          line.sep('', theme.win, theme.fill),
-          win.is_current() and '' or '',
-          win.buf_name(),
-          line.sep('', theme.win, theme.fill),
-          hl = theme.win,
           margin = ' ',
         }
       end),
-      {
-        line.sep('', theme.tail, theme.fill),
-        { '  ', hl = theme.tail },
-      },
+      line.spacer(),
+--      line.wins_in_tab(line.api.get_current_tab()).foreach(function(win)
+--        return {
+--          line.sep('', theme.win, theme.fill),
+--          win.is_current() and '' or '',
+--          win.buf_name(),
+--          line.sep('', theme.win, theme.fill),
+--          hl = theme.win,
+--          margin = ' ',
+--        }
+--      end),
+--      {
+--        line.sep('', theme.tail, theme.fill),
+--        { '  ', hl = theme.tail },
+--      },
       hl = theme.fill,
     }
   end,
