6a5cfbd8e2242b4f431a34b62dac491194c527e0
Creativ <vietbao.telescope@gmail.com>
2025-09-20 15:25:10 +0700

    up

--- windows/.vim/lua/neo-init.lua
+++ windows/.vim/lua/neo-init.lua
@@ -15,44 +15,30 @@ local theme = {
   win = 'TabLine',
   tail = 'TabLine',
 }
-require('tabby').setup({
-
-
-  line = function(line)
-    return {
-      {
-        { '  ', hl = theme.head },
-        line.sep('', theme.head, theme.fill),
-      },
-      line.tabs().foreach(function(tab)
-        local hl = tab.is_current() and theme.current_tab or theme.tab
-        return {
-          line.sep('', hl, theme.fill),
-          tab.is_current(),
-          tab.name(),
---          tab.close_btn(''),
-          line.sep('', hl, theme.fill),
-          hl = hl,
-          margin = ' ',
-        }
-      end),
-      line.spacer(),
---      line.wins_in_tab(line.api.get_current_tab()).foreach(function(win)
---        return {
---          line.sep('', theme.win, theme.fill),
---          win.is_current() and '' or '',
---          win.buf_name(),
---          line.sep('', theme.win, theme.fill),
---          hl = theme.win,
---          margin = ' ',
---        }
---      end),
---      {
---        line.sep('', theme.tail, theme.fill),
---        { '  ', hl = theme.tail },
---      },
-      hl = theme.fill,
-    }
-  end,
-  -- option = {}, -- setup modules' option,
-})
+require('tabby').setup(
+  {
+    line = function(line)
+      return {
+        {
+          { '  ', hl = theme.head },
+          line.sep('', theme.head, theme.fill),
+        },
+        line.tabs().foreach(function(tab)
+          local hl = tab.is_current() and theme.current_tab or theme.tab
+          return {
+            line.sep('', hl, theme.fill),
+            tab.is_current(),
+            tab.name():gsub('%s*%[.+%]', ''),
+--            tab.close_btn(''),
+            line.sep('', hl, theme.fill),
+            hl = hl,
+            margin = ' ',
+          }
+        end),
+        line.spacer(),
+        hl = theme.fill,
+      }
+    end,
+    option = {}
+  }
+)
