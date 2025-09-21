8dfd5ba8f8e5b4e0cdc88cb458afa4cc05da4c0b
Creativ <vietbao.telescope@gmail.com>
2025-09-21 07:58:42 +0700

    VIM-21

--- windows/.vim/lua/neo-init.lua
+++ windows/.vim/lua/neo-init.lua
@@ -1,4 +1,45 @@
 require("nvim-web-devicons")
-require('lualine').setup()
-require('lualine.themes.gruvbox_light')
+require('lualine').setup(
+--  {options = { theme = 'nord' }}
+--  {options = { theme = 'papercolor_dark' }}
+--  {options = { theme = 'solarized_dark' }}
+--  {options = { theme = 'solarized_light' }}
+)
 
+local theme = {
+  fill = 'TabLineFill',
+  -- Also you can do this: fill = { fg='#f2e9de', bg='#907aa9', style='italic' }
+  head = { fg='#ffffff', bg='#000000', style='italic' },
+  current_tab = { fg='#000000', bg='#f5006e', style='italic' },
+  tab = 'TabLine',
+  win = 'TabLine',
+  tail = 'TabLine',
+}
+require('tabby').setup(
+  {
+    line = function(line)
+      return {
+        {
+          { '  ', hl = theme.head },
+          line.sep('', theme.head, theme.fill),
+        },
+        line.tabs().foreach(function(tab)
+          local hl = tab.is_current() and theme.current_tab or theme.tab
+          return {
+            line.sep('', hl, theme.fill),
+            tab.is_current(),
+--            tab.name():gsub('%s*%[.+%]', ''),
+            tab.number(),
+--            tab.close_btn(''),
+            line.sep('', hl, theme.fill),
+            hl = hl,
+            margin = ' ',
+          }
+        end),
+        line.spacer(),
+        hl = theme.fill,
+      }
+    end,
+    option = {}
+  }
+)
